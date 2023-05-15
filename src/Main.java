d694bae4d54ce80a3d0f9e8a384d4dc1ad8c3135
Jitse-Hoefnagel <jitsehoef@gmail.com>
2023-05-15 12:09:14 +0200

    I guess this works?

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("");
-
-//        for (int i = 1; i <= 5; i++) {
-//
-//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-//            // for you, but you can always add more by pressing Ctrl+F8.
-//            System.out.println("i = " + i);
-//        }
+        System.out.println("Hi, My name is Jitse and I'll tell you something about myself");
+        System.out.println("I'm a 26 year old guy from Tiel...");
     }
 }

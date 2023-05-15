61b013ed4c92a8429ca192068df228411d4adbbb
Jitse-Hoefnagel <jitsehoef@gmail.com>
2023-05-15 12:25:09 +0200

    I guess this works?

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, My name is Jitse and I'll tell you something about myself");
         System.out.println("I'm a 26 year old guy from Tiel...");
+        System.out.println("");
     }
 }
